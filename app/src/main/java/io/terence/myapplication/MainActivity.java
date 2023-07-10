02e32bf63a5a810cb596e7eda73f1678b393bcef
Terence Pan <tpan6@wgu.edu>
2023-07-10 16:57:22 -0700

    Continue Checkin for 1.  Create a user option to enter, update and delete vacations.
    a.  Allow the user to add as many vacations as desired. and

--- app/src/main/java/io/terence/myapplication/MainActivity.java
+++ app/src/main/java/io/terence/myapplication/MainActivity.java
@@ -1,14 +1,41 @@
 package io.terence.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.List;
+
+import io.terence.myapplication.vacations.Vacation;
+import io.terence.myapplication.vacations.VacationDao;
+import io.terence.myapplication.vacations.VacationViewAdapter;
+
 public class MainActivity extends AppCompatActivity {
 
+    private RecyclerView recyclerView;
+    private VacationDao vacationDao;
+    private VacationViewAdapter vacationViewAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        loadTableData();
+    }
+
+
+    private void loadTableData() {
+        new Thread(() -> {
+            List<Vacation> entityList = vacationDao.getAllEntities();
+            runOnUiThread(() -> {
+                vacationViewAdapter = new VacationViewAdapter(entityList);
+                recyclerView.setAdapter(vacationViewAdapter);
+            });
+        }).start();
     }
 }
